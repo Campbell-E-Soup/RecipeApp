49dce46a311334d95cea53dfe49f4c2a8c89be97
Campbell-E-Soup <cechudley@pstcc.edu>
2025-03-21 17:39:43 -0400

    all functionality complete

--- app/src/main/java/com/example/recipeapp/MainActivity.kt
+++ app/src/main/java/com/example/recipeapp/MainActivity.kt
@@ -4,44 +4,50 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.recipeapp.model.Recipe
+import com.example.recipeapp.ui.RecipeApp
 import com.example.recipeapp.ui.theme.RecipeAppTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             RecipeAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                val layoutDirection = LocalLayoutDirection.current
+                Surface(
+                    modifier = Modifier
+                        .padding(
+                            start = WindowInsets.safeDrawing.asPaddingValues()
+                                .calculateStartPadding(layoutDirection),
+                            end = WindowInsets.safeDrawing.asPaddingValues()
+                                .calculateEndPadding(layoutDirection)
+                        )
+                ) {
+                    val windowSize = calculateWindowSizeClass(this)
+                    RecipeApp(
+                        windowSize = windowSize.widthSizeClass,
+                        onBackPressed = { finish() }
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RecipeAppTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/example/recipeapp/ui/RecipeScreen.kt
+++ app/src/main/java/com/example/recipeapp/ui/RecipeScreen.kt
@@ -21,7 +21,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -29,48 +28,115 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.recipeapp.R
+import com.example.recipeapp.model.Recipe
+import com.example.recipeapp.utils.RecipesContentType
 
 
 @Composable
 fun RecipeApp(
-    modifier: Modifier = Modifier
+    windowSize: WindowWidthSizeClass,
+    onBackPressed: () -> Unit,
 ) {
     val viewModel: RecipeViewModel = viewModel()
     val uiState by viewModel.uiState.collectAsState()
 
+    val contentType = when (windowSize) {
+        WindowWidthSizeClass.Compact,
+        WindowWidthSizeClass.Medium -> RecipesContentType.ListOnly
 
+        WindowWidthSizeClass.Expanded -> RecipesContentType.ListAndDetail
+        else -> RecipesContentType.ListOnly
+    }
+
+    Scaffold(
+        topBar = {
+            RecipeAppBar(
+                isShowingListPage = uiState.isShowingListPage,
+                onBackButtonClick = { viewModel.navigateToListPage() },
+                windowSize = windowSize
+            )
+        }
+    ) { innerPadding ->
+        if (contentType == RecipesContentType.ListAndDetail) {
+            SportsListAndDetail(
+                recipes = uiState.recipesList,
+                selectedRecipe = uiState.currentRecipe,
+                onClick = {
+                    viewModel.updateCurrentSport(it)
+                },
+                onBackPressed = onBackPressed,
+                contentPadding = innerPadding,
+                modifier = Modifier.fillMaxWidth()
+            )
+        } else {
+            if (uiState.isShowingListPage) {
+                RecipeList(
+                    recipes = uiState.recipesList,
+                    onClick = {
+                        viewModel.updateCurrentSport(it)
+                        viewModel.navigateToDetailPage()
+                    },
+                    modifier = Modifier.padding(horizontal = 16.dp),
+                    contentPadding = innerPadding,
+                )
+            } else {
+                RecipeDetails(
+                    selectedRecipe = uiState.currentRecipe,
+                    contentPadding = innerPadding,
+                    onBackPressed = {
+                        viewModel.navigateToListPage()
+                    }
+                )
+            }
+        }
+    }
 }
 //app bar
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RecipeAppBar(
     onBackButtonClick: () -> Unit,
-    isShowingListPage:Boolean,
-    modifier: Modifier
+    isShowingListPage: Boolean,
+    windowSize: WindowWidthSizeClass,
+    modifier: Modifier = Modifier
 ) {
-    //dynamic appbar
+    val isShowingDetailPage = windowSize != WindowWidthSizeClass.Expanded && !isShowingListPage
     TopAppBar(
         title = {
             Text(
-                text = if (!isShowingListPage) {
+                text =
+                if (isShowingDetailPage) {
                     stringResource(R.string.detail_fragment_label)
                 } else {
                     stringResource(R.string.list_fragment_label)
-                }
+                },
+                fontWeight = FontWeight.Bold
             )
         },
-        navigationIcon = if (!isShowingListPage) {
+        navigationIcon = if (isShowingDetailPage) {
             {
                 IconButton(onClick = onBackButtonClick) {
                     Icon(
@@ -82,7 +148,6 @@ fun RecipeAppBar(
         } else {
             { Box {} }
         },
-        //TODO change colors in theme
         colors = TopAppBarDefaults.topAppBarColors(
             containerColor = MaterialTheme.colorScheme.primary
         ),
@@ -90,3 +155,166 @@ fun RecipeAppBar(
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun RecipeListItem( //how recipes and their information is displayed
+    recipe: Recipe,
+    onItemClick: (Recipe) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        elevation = CardDefaults.cardElevation(),
+        modifier = modifier,
+        shape = RoundedCornerShape(dimensionResource(R.dimen.card_corner_radius)),
+        onClick = { onItemClick(recipe) }
+    )
+    {
+        Row(modifier = modifier
+            .fillMaxWidth()
+            .size(dimensionResource(R.dimen.card_inner_size))) {
+            Column(
+                modifier = Modifier
+                    .padding(
+                        vertical = 8.dp,
+                        horizontal = 16.dp
+                    )
+                    .weight(1f)
+            ) {
+                Text(
+                    text = stringResource(recipe.nameResourceId),
+                    style = MaterialTheme.typography.titleMedium,
+                    modifier = Modifier.padding(bottom = 4.dp)
+                )
+                Text(
+                    text = stringResource(recipe.descResourceId),
+                    style = MaterialTheme.typography.bodySmall,
+                    color = MaterialTheme.colorScheme.secondary,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 3
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun RecipeList( //displays recipes as cards in a lazy column
+    recipes: List<Recipe>,
+    onClick: (Recipe) -> Unit,
+    modifier: Modifier = Modifier,
+    contentPadding: PaddingValues = PaddingValues(2.dp),
+) {
+    LazyColumn(
+        contentPadding = contentPadding,
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        modifier = modifier,
+    ) {
+        items(recipes, key = { recipe -> recipe.id }) { recipe ->
+            RecipeListItem(
+                recipe = recipe,
+                onItemClick = onClick
+            )
+        }
+    }
+}
+
+@Composable
+fun RecipeDetails(
+    selectedRecipe: Recipe,
+    onBackPressed: () -> Unit,
+    contentPadding: PaddingValues,
+    modifier: Modifier = Modifier
+) {
+    BackHandler {
+        onBackPressed()
+    }
+    val scrollState = rememberScrollState()
+    val layoutDirection = LocalLayoutDirection.current
+    Box(
+        modifier = modifier
+            .verticalScroll(state = scrollState)
+            .padding(top = contentPadding.calculateTopPadding())
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(
+                    bottom = contentPadding.calculateTopPadding(),
+                    start = contentPadding.calculateStartPadding(layoutDirection),
+                    end = contentPadding.calculateEndPadding(layoutDirection)
+                )
+        ) {
+            Box {
+                Column(
+                    Modifier
+                        .align(Alignment.BottomStart)
+                        .fillMaxWidth()
+                ) {
+                    Text(
+                        text = stringResource(selectedRecipe.nameResourceId),
+                        style = MaterialTheme.typography.headlineLarge,
+                        color = MaterialTheme.colorScheme.primary,
+                        modifier = Modifier
+                            .padding(horizontal = 8.dp)
+                    )
+                }
+            }
+            Text(
+                text = stringResource(selectedRecipe.descResourceId),
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier.padding(
+                    vertical = 24.dp,
+                    horizontal = 40.dp
+                )
+            )
+        }
+    }
+}
+
+@Composable
+private fun SportsListAndDetail(
+    recipes: List<Recipe>,
+    selectedRecipe: Recipe,
+    onClick: (Recipe) -> Unit,
+    onBackPressed: () -> Unit,
+    modifier: Modifier = Modifier,
+    contentPadding: PaddingValues = PaddingValues(0.dp),
+) {
+    Row(
+        modifier = modifier
+    ) {
+        RecipeList(
+            recipes = recipes,
+            onClick = onClick,
+            contentPadding = PaddingValues(
+                top = contentPadding.calculateTopPadding(),
+            ),
+            modifier = Modifier
+                .weight(2f)
+                .padding(16.dp)
+        )
+        RecipeDetails(
+            selectedRecipe = selectedRecipe,
+            modifier = Modifier.weight(3f),
+            contentPadding = PaddingValues(
+                top = contentPadding.calculateTopPadding(),
+            ),
+            onBackPressed = onBackPressed,
+        )
+    }
+}
+
+
+@Preview
+@Composable
+fun ListItemPreview() {
+    RecipeListItem(onItemClick = {}, recipe = Recipe(id = 0, nameResourceId = R.string.devil_food,R.string.dev_desc))
+}
+
+@Preview
+@Composable
+fun DetailPreview() {
+    RecipeDetails(selectedRecipe = Recipe(id = 0, nameResourceId = R.string.devil_food,R.string.dev_desc),
+        onBackPressed = {},
+        contentPadding = PaddingValues(8.dp)
+        )
+}
